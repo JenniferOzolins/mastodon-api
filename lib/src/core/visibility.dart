bf0c273716addf8f1b848fef5ee8a77d046704cc
Jenni <j.ozolins@28apps.de>
2024-01-23 14:43:07 +0100

    next try fixing visibility isn't json encodable

--- lib/src/core/visibility.dart
+++ lib/src/core/visibility.dart
@@ -3,26 +3,21 @@
 // modification, are permitted provided the conditions.
 
 // 🌎 Project imports:
-import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'serializable.dart';
 
 /// Represents the visibility of specific status.
 enum Visibility implements Serializable {
   /// Visible to everyone, shown in public timelines.
-  @JsonValue('public')
   public('public'),
 
   /// Visible to public, but not included in public timelines.
-  @JsonValue('unlisted')
   unlisted('unlisted'),
 
   /// Visible to followers only, and to any mentioned users.
-  @JsonValue('private')
   private('private'),
 
   ///  Visible only to mentioned users.
-  @JsonValue('direct')
   direct('direct');
 
   @override
